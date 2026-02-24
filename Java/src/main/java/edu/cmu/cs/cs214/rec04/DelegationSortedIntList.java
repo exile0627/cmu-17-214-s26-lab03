bcfa0a32b49206c7d2674c42dfff8ef8114b42e6
lorna <374695638@qq.com>
2026-02-23 21:51:59 -0500

    task2 finished

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
@@ -10,7 +10,44 @@ package edu.cmu.cs.cs214.rec04;
  *
  */
 
-public class DelegationSortedIntList {
+public class DelegationSortedIntList implements IntegerList{
     // Write your implementation below with API documentation
+    private int totalAdded = 0;
+    private SortedIntList sortedIntList = new SortedIntList();
+    
+    @Override
+    public boolean add(int num) {
+        this.totalAdded++;
+        return sortedIntList.add(num);
+    }
 
+    @Override
+    public boolean addAll(IntegerList list) {
+        this.totalAdded += list.size();
+        return sortedIntList.addAll(list);
+    }
+
+    @Override
+    public int get(int index) {
+        return sortedIntList.get(index);
+    }
+
+    @Override
+    public boolean remove(int num) {
+        return sortedIntList.remove(num);
+    }
+
+    @Override
+    public boolean removeAll(IntegerList list) {
+        return sortedIntList.removeAll(list);
+    }
+
+    @Override
+    public int size() {
+        return sortedIntList.size();
+    }
+
+    public int getTotalAdded() {
+        return this.totalAdded;
+    }
 }
